a04153d7eb62f644b6c825eee271f7da13f6d1f5
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-10-23 14:55:28 +0100

    WE-11 feature: teams crud done with tests

--- wolfesports/src/main/java/repository/implementation/TeamRepositoryImpl.java
+++ wolfesports/src/main/java/repository/implementation/TeamRepositoryImpl.java
@@ -3,39 +3,126 @@ package repository.implementation;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import model.Team;
 import repository.interfaces.TeamRepository;
+import util.PersistenceUtil;
 
-public class TeamRepositoryImpl implements TeamRepository{
+public class TeamRepositoryImpl implements TeamRepository {
+    private static final Logger logger = LoggerFactory.getLogger(TeamRepositoryImpl.class);
 
     @Override
     public Optional<Team> get(long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        Team team = null;
+        try {
+            TypedQuery<Team> typedQuery;
+
+            typedQuery = entityManager.createQuery(
+                    "SELECT DISTINCT t FROM Team t LEFT JOIN FETCH t.players p WHERE (p.isDeleted = false OR p.id IS NULL) AND t.id = :id ORDER BY t.id ASC",
+                    Team.class);
+
+            typedQuery.setParameter("id", id);
+
+            team = typedQuery.getSingleResult();
+
+        } catch (Exception e) {
+            logger.error("Error getting team: ", e);
+        } finally {
+            entityManager.close();
+        }
+
+        return Optional.ofNullable(team);
     }
 
     @Override
     public List<Team> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        List<Team> teams = null;
+
+        try {
+            TypedQuery<Team> typedQuery;
+
+            typedQuery = entityManager.createQuery(
+                    "SELECT DISTINCT t FROM Team t ORDER BY t.id ASC",
+                    Team.class);
+
+            teams = typedQuery.getResultList();
+
+        } catch (Exception e) {
+            logger.error("Error listing teams: ", e);
+        } finally {
+            entityManager.close();
+        }
+
+        return teams;
     }
 
     @Override
     public void save(Team team) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(team);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            logger.error("Error creating team: ", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void update(Team team) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.merge(team);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            logger.error("Error creating team: ", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void delete(long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            Team team = entityManager.find(Team.class, id);
+            if (team != null) {
+                team.setIsDeleted(true);
+                entityManager.merge(team);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            logger.error("Error creating team: ", e);
+        } finally {
+            entityManager.close();
+        }
     }
-    
+
 }
--- wolfesports/src/main/java/service/TeamService.java
+++ wolfesports/src/main/java/service/TeamService.java
@@ -1,5 +1,9 @@
 package service;
 
+import java.util.List;
+import java.util.Optional;
+
+import model.Team;
 import repository.interfaces.TeamRepository;
 
 public class TeamService {
@@ -9,4 +13,25 @@ public class TeamService {
     public TeamService(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
     }
+
+        public Optional<Team> getTeam(long id) {
+		return teamRepository.get(id);
+	}
+
+	public List<Team> getAllTeams() {
+		return teamRepository.getAll();
+	}
+
+
+	public void addTeam(Team team) {
+		teamRepository.save(team);
+	}
+
+	public void updateTeam(Team team) {
+		teamRepository.update(team);
+	}
+
+	public void deleteTeam(long id) {
+		teamRepository.delete(id);
+	}
 }
